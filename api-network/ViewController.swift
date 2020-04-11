14e3644178338d5e0f6d0c4b08d1a889f99bf1fd
erick.silva <erick.silva@c6bank.com>
2020-04-11 15:09:15 -0300

    Initial Commit

---
+++ api-network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  api-network
+//
+//  Created by Erick Kaique Da Silva on 11/04/20.
+//  Copyright © 2020 Erick Kaique Da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
